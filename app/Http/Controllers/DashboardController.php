9262624a2995dcd04e55c68afdf19f9e5f0c18d2
Grant Brivael <granttiwa0@gmail.com>
2024-05-24 14:39:50 +0100

    jkjh

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\AnswerRepository;
 use Illuminate\Http\Request;
 use App\Repositories\QuestionnaireRepository;
+use App\Repositories\UserRepository;
 
 class DashboardController extends Controller
 {
     private $questionnaireRepository;
-    public function __construct(QuestionnaireRepository $questionnaireRepository){
+    private $answerRepository;
+    private $userRepository;
+    public function __construct(QuestionnaireRepository $questionnaireRepository,
+            AnswerRepository $answerRepository, UserRepository $userRepository){
         $this->questionnaireRepository = $questionnaireRepository;
+        $this->answerRepository = $answerRepository;
+        $this->userRepository = $userRepository;
     }
 
     public function dashboard(){
         $questionnaires = $this->questionnaireRepository->getAll(2);
+        $publishQuestionnaires = $this->questionnaireRepository->getAll();
 
-        return view('dashboard', compact('questionnaires'));
+        $responseQuestionnaire = $this->answerRepository->getAll();
+        $answers = $this->answerRepository->getAll();
+        $users = $this->userRepository->getAll();
+
+        return view('dashboard', compact('users', 'questionnaires','publishQuestionnaires', 'responseQuestionnaire','answers'));
     }
 }
