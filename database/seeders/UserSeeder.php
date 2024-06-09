10306563d5eb7376607ad83f173e3ad92a5d24d1
Grant Brivael <granttiwa0@gmail.com>
2024-06-09 16:37:45 +0100

    seed admin user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = array(
+            array(
+                'name' => "admin",
+                'email' => 'admin@admin.com2',
+                'password' => Hash::make('admin@admin.com'),
+            )
+        );
+
+        foreach ($users as $user) {
+            $exitUser = User::where('email', $user['email'])->first();
+
+            if (!$exitUser) {
+                User::create($user);
+            }
+        }
+    }
+}
